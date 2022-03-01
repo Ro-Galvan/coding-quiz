2dc1050c66c52109c5ab83b381f7217073342763
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-02-28 21:22:53 -0500

    added a timer

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-console.log('does this work??');
+// function startGame (){
+// // setInterval(function()  {
+// // console.log('hello')    
+// // }, 1000);
+// };
+var counter = 5
+var myTimer = setInterval(function()  {
+
+    if (counter <= 0) clearInterval(myTimer) 
+    console.log(counter--)    
+    }, 1000);
+
+
+// //array of questions
+// var questions = [
+// {
+//     prompt: "Where should you put the script tag in your html file?"
+// }
+// ]
+
+// console.log('questions')
+// function score{};
+// function form{};
+// function timer{};
+
+// start button----done but need to --google javascript replace variable on page
+// start timer
+    // remove inital elements from page 
+    // display 1st question
+        // display the 4 choices 
+        // handle the answer based on choice clicked
+        // if wrong, penalize score time by ten seconds
+        // else correct, move on to next question
+        // after last question stop the timer
+//  at the end save/submit initials ---form input
+// clear score/go back---- 
+// give option to view past scores --- local storage 
+
+
