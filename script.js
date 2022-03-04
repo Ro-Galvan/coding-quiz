f7e667579aebf30c8ec7e5fbcb17eac4d301154f
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-03-03 21:12:43 -0500

    created a var so questions  would display on screen

--- script.js
+++ script.js
@@ -1,89 +1,121 @@
-var contentEl = document.getElementsByClassName('.content');
-var questionsMenuEl = document.getElementById('#questions-menu');
-var endMenuEl = document.getElementById('#end-menu');
-
-var startButton = document.getElementById('#start-button');
-var formInitials = document.getElementById('#initials');
-
-var cursor = 0;
+var contentEl = document.querySelector('.content');
+var questionsEl = document.querySelector('#questions');
+var endMenuEl = document.querySelector('#end-menu');
+var startButton = document.querySelector('#start-button');
+var formInitials = document.querySelector('#initials');
+console.log(startButton);
+var currentQNum = 0;
 
 var questions = [
 {
-Q: "Where do you place the script tag for external js files?",
-  A: ['in the head of the html file', 
+q: "Where do you place the script tag for external js files?",
+  a: ['in the head of the html file', 
     'it links itself',
     'after the opening body tag in the html file', 
     'before the closing body tag in the html file'],
-  correct: 2
-}
+  correct: 4
+},
+
+{
+  q: "Where do you place the script tag for external js files?",
+    a: ['in the head of the html file', 
+      'it links itself',
+      'after the opening body tag in the html file', 
+      'before the closing body tag in the html file'],
+    correct: 2
+  },
+
+  {
+    q: "Where do you place the script tag for external js files?",
+      a: ['in the head of the html file', 
+        'it links itself',
+        'after the opening body tag in the html file', 
+        'before the closing body tag in the html file'],
+      correct: 1
+    },
+
+    {
+      q: "Where do you place the script tag for external js files?",
+        a: ['in the head of the html file', 
+          'it links itself',
+          'after the opening body tag in the html file', 
+          'before the closing body tag in the html file'],
+        correct: 3
+      },
+
+      {
+        q: "Where do you place the script tag for external js files?",
+          a: ['in the head of the html file', 
+            'it links itself',
+            'after the opening body tag in the html file', 
+            'before the closing body tag in the html file'],
+          correct: 2
+        },
 ];
 
 //instructor provided to help
 function startScreen() {
   contentEl.style.display = "block";
-  questionsMenuEl.style.display = "none";
+  questionsEl.style.display = "none";
   endMenuEl.style.display = "none";
 }
-
+console.log('does this work-startscreen??');
 function gameScreen() {
-  contentEl.style.display = "none";
-  questionsMenuEl.style.display = "block";
+  // contentEl.style.display = "none";
+  questionsEl.style.display = "block";
   endMenuEl.style.display = "none";
   displayQuestion();
 }
-
+console.log('does this work-gamescreen??');
 
 function displayQuestion() {
-    var qestn = questions[cursor];
-    questions
-  for (var i = 0; i < qestn.choices.length; i++) {
-    var item = qestn.A[i];
+  questionsEl.innerHTML = '';
+    var questionSelected = questions[currentQNum]; //take ? array and grab element that is position [0]
+    var div = document.createElement('div'); //this needs to be a tag 
+     div.textContent = questionSelected.q
+    questionsEl.appendChild(div);
+  for (var i = 0; i < questionSelected.a.length; i++) {
+    var item = questionSelected.a[i];
     var btn = document.createElement('button');
     btn.textContent = i + 1 + ". " + item;
-    questionsMenuEl.appendChild(btn);
+    // btn.value = item;
+    questionsEl.appendChild(btn);
   }
+  console.log('does this work-display question??');
 }
 
 function endScreen() {
   contentEl.style.display = "none";
-  questionsMenuEl.style.display = "none";
+  questionsEl.style.display = "none";
   endMenuEl.style.display = "block";
 }
 //submit initials ---form input
-function form() {
-    var formInitials = document.getElementById('#initials');
+function forminput() {
+    var formInitials = document.querySelector('#initials');
 
     // will remove initials after submit is clicked
     formInitials.innerhtml = ""; 
 }
-
 
 function init() {
   startScreen();
 }
 
 startButton.addEventListener('click', gameScreen);
-questionsMenuEl.addEventListener('click', endScreen);
-
+//Removed endscreen with function (event)
+questionsEl.addEventListener('click', function (event) {
+  if (event.target.matches('btn')) {
+    console.log(event.target);
+    cursor++;
+    if (cursor < questions.length) {
+      displayQuestion();
+    } else {
+      endScreen();
+    }
+  }
+});
 
 init();
-
-
-// var questions = {
-// { 
-//     jsQuestion: "Where do you place the script tag for external js files?",
-//     choices: ['in head of the html file', 'it links itself, 'after the opening body tag in the html file', 'before the closing body tag in the html file' 
-// ],
-//     answer: '3'
-// },
-// }
-//    console.log(does it work?);
-// var questions = [
-//     {
-//         title: "Commonly used data types DO NOT include:",
-//         choices: ["strings", "booleans", "alerts", "numbers"],
-//         answer: "alerts"
-//     },
 
 function startGame(){
 setInterval(function()  {
@@ -97,8 +129,6 @@ var myTimer = setInterval(function()  {
     if (counter <= 0) clearInterval(myTimer) 
     console.log(counter--)    
     }, 1000);
-
-
 
 /*start button----done but need to --google javascript replace variable on page
 start timer
